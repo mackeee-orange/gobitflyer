04e827c790e493337967b83374fbd7efac18798a
Akihiro Makino <makino.aaa@gmail.com>
2017-01-25 18:06:04 +0900

    first commit

---
+++ gobitflyer/gobitflyer.go
@@ -0,0 +1,24 @@
+//package gobitflyer
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type API interface {
+
+}
+
+func main(){
+	uri := "https://api.bitflyer.jp/v1/gethealth"
+	req, _ := http.NewRequest("GET", uri, nil)
+
+	client := new(http.Client)
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(byteArray))
+}
